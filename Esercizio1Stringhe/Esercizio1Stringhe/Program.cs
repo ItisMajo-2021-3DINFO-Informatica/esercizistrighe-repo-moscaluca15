da60127b4a39d2eccf214265a81b1151ba2d26b1
moscaluca15 <98151019+moscaluca15@users.noreply.github.com>
2022-02-11 10:08:07 +0100

    esercizio3

---
+++ Esercizio1Stringhe/Esercizio1Stringhe/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Esercizio1Stringhe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Esercizi Stringhe 3");
+            string parola = "";
+            int contaparole = 0;
+            while (parola != "fine")
+            {
+                Console.WriteLine("inserisci una parola");
+                parola = Console.ReadLine();
+                char prima = parola[0];
+                char ultima = parola[lunghezza - 1];
+                int lunghezza = parola.Length;
+                if (prima == ultima)
+                {
+                    contaparole++;
+                }
+                Console.WriteLine("Ci sono che iniziano" + parola + "e finiscono " + ultima);
+            }
+        }
+    }
+}
